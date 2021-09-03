fb95888a4c443cd23d613e75e2f3dff991c90a12
Dean Marx <deanwmarx@gmail.com>
2021-09-03 22:05:59 +0200

    added more blade components to make dynamic blog post grid layouts using css grid and tailwind css

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,22 +2,13 @@
     @include('_posts-header')
     
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-post-featured-card />
-
-    <div class="lg:grid lg:grid-cols-2">
-        <x-post-card />
-        <x-post-card />
-    </div>
-
-    <div class="lg:grid lg:grid-cols-3">
-        
-        <x-post-card />
-        <x-post-card />
-        <x-post-card />
-        
-    </div>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p>No posts yet. Please check back later.</p>
+        @endif)
     </main>
-    {{-- @foreach ($posts as $post)
+    {{-- @foreach ($posts as $post)  // Old way of looping through database content
         <article>
             <h1>
                 <a href="/posts/{{ $post->slug }}">
