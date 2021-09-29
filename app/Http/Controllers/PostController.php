0573790ffbe8392c3130e6035fe2b05794466108
Dean Marx <deanwmarx@gmail.com>
2021-09-29 17:00:23 +0200

    Extracted tha Category dropdown to its own Component. Then cleaned up the routes file as well as the PostController code by moving the Category queries to the Component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,18 +11,15 @@ class PostController extends Controller
     public function index() {
         
 
-        return view('posts', [
-            // 'posts' => Post::latest()->with(['category', 'author'])->get(),
+        return view('posts.index', [
             'posts' => Post::latest()
                 ->filter(request(['search', 'category']))
                 ->get(),
-            'categories' => Category::all(),
-            'currentCategory' => Category::firstWhere('slug', request('category'))
         ]);
     }
 
     public function show(Post $post) {
-        return view('post', [
+        return view('posts.show', [
             'post'=> $post
         ]);
     }
