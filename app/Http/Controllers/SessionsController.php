b29e05cd58d93fa30b3170d448a8307172a21469
Dean Marx <deanwmarx@gmail.com>
2021-10-05 22:28:31 +0200

    built a log in page as well as logic to validate the username and password

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        // attempt to authenticate and log in the user
+        // based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            // session fixation
+            session()->regenerate();
+            //redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // auth failed
+        // return back() // manual way to fail validation
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
