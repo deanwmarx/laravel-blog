5ed874772309b1951016adb0f36e80b6f35d8e7e
Dean Marx <deanwmarx@gmail.com>
2021-10-19 22:54:28 +0200

    added basic support for admin functionality to publish a new post using middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Post;
+use Illuminate\Validation\Rule;
 
 class PostController extends Controller
 {
@@ -22,5 +22,25 @@ class PostController extends Controller
         return view('posts.show', [
             'post'=> $post
         ]);
+    }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('posts', 'slug')],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        $attributes['user_id'] = auth()->id();
+
+        Post::create($attributes);
+
+        return redirect('/');
     }
 }
